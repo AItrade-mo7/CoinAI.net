3ce3c39ed8fb5ffa0f650bdeaa3fb191912bb8cd
墨七 <meichangliang@mo7.cc>
2022-07-15 23:59:48 +0800

    exit-push

--- server/router/middle/EncryptAuth.go
+++ server/router/middle/EncryptAuth.go
@@ -12,7 +12,6 @@ func EncryptAuth(c *fiber.Ctx) error {
 	EncStr := c.Get("Auth-Encrypt")
 	shaStr := config.Encrypt(c.Path() + c.Get("User-Agent"))
 	isFind := strings.Contains(shaStr, EncStr)
-
 	if !isFind {
 		return errors.New("授权验证错误")
 	}
