31806952bb14b171638f78c3c453310b5eb4f489
墨七 <meichangliang@mo7.cc>
2022-07-22 02:15:41 +0800

    exit-push

--- server/okxApi/wssApi/Fetch.go
+++ server/okxApi/wssApi/Fetch.go
@@ -1,16 +1,12 @@
-package restApi
+package wssApi
 
 import (
-	"fmt"
-	"strings"
-
 	"CoinAI.net/server/global/dbData"
 	"github.com/EasyGolang/goTools/mEncrypt"
 	"github.com/EasyGolang/goTools/mFetch"
 	"github.com/EasyGolang/goTools/mJson"
 	"github.com/EasyGolang/goTools/mStr"
 	"github.com/EasyGolang/goTools/mTime"
-	"github.com/EasyGolang/goTools/mUrl"
 )
 
 /*
@@ -32,66 +28,54 @@ import (
 	fmt.Println(mStr.ToStr(resData))
 
 */
-
-type FetchOpt struct {
-	Path   string
-	Data   map[string]any
-	Method string
-	Event  func(string, any)
+type LoginArgsType struct {
+	APIKey     string `json:"apiKey"`
+	Passphrase string `json:"passphrase"`
+	Timestamp  string `json:"timestamp"`
+	Sign       string `json:"sign"`
+}
+type LoginType struct {
+	Op   string          `json:"op"`
+	Args []LoginArgsType `json:"args"`
 }
 
-func Fetch(opt FetchOpt) (resData []byte, resErr error) {
-	if len(opt.Method) < 1 {
-		opt.Method = "GET"
+type FetchOpt struct {
+	Type  int
+	Event func(string, any)
+}
+
+func New(opt FetchOpt) (_this *mFetch.Wss) {
+	WssOpt := mFetch.WssOpt{}
+	WssOpt.Event = opt.Event
+	if opt.Type == 0 {
+		WssOpt.Url = "wss://ws.okx.com:8443/ws/v5/public"
 	}
+	if opt.Type == 1 {
+		WssOpt.Url = "wss://ws.okx.com:8443/ws/v5/private"
+	}
+	_this = mFetch.NewWss(WssOpt)
 
-	// 处理 Header 和 加密信息
-	Method := strings.ToUpper(opt.Method)
-	Timestamp := mTime.IsoTime(true)
-	ApiKey := dbData.OkxKey.ApiKey
-	SecretKey := dbData.OkxKey.SecretKey
-	Passphrase := dbData.OkxKey.Passphrase
-	Body := mJson.ToJson(opt.Data)
-
-	SignStr := mStr.Join(
-		Timestamp,
-		Method,
-		opt.Path,
-		string(Body),
-	)
-
-	if Method == "GET" {
-		Body = []byte("")
-		urlO := mUrl.InitUrl(opt.Path)
-		for key, val := range opt.Data {
-			v := fmt.Sprintf("%+v", val)
-			urlO.AddParam(key, v)
-		}
-		signPath := urlO.String()
-		SignStr = mStr.Join(
+	if opt.Type == 1 {
+		Timestamp := mTime.EpochTime()
+		SignStr := mStr.Join(
 			Timestamp,
-			Method,
-			signPath,
-			string(Body),
+			"GET",
+			"/users/self/verify",
 		)
+		Sign := mEncrypt.Sha256(SignStr, dbData.OkxKey.SecretKey)
+		LoginParam := LoginType{
+			Op: "",
+			Args: []LoginArgsType{
+				{
+					APIKey:     dbData.OkxKey.ApiKey,
+					Passphrase: dbData.OkxKey.Passphrase,
+					Timestamp:  Timestamp,
+					Sign:       Sign,
+				},
+			},
+		}
+		_this.Write(mJson.ToJson(LoginParam))
 	}
-	Sign := mEncrypt.Sha256(SignStr, SecretKey)
-	fetch := mFetch.NewHttp(mFetch.HttpOpt{
-		Origin: "https://www.okx.com",
-		Path:   opt.Path,
-		Data:   opt.Data,
-		Event:  opt.Event,
-		Header: map[string]string{
-			"OK-ACCESS-KEY":        ApiKey,
-			"OK-ACCESS-SIGN":       Sign,
-			"OK-ACCESS-TIMESTAMP":  Timestamp,
-			"OK-ACCESS-PASSPHRASE": Passphrase,
-		},
-	})
 
-	if Method == "GET" {
-		return fetch.Get()
-	} else {
-		return fetch.Post()
-	}
+	return
 }
