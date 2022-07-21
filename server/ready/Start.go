7fb1cc51d2a693d8fe37be77e8afb5fb9be98d08
墨七 <meichangliang@mo7.cc>
2022-07-21 23:58:26 +0800

    exit-push

--- server/ready/Start.go
+++ server/ready/Start.go
@@ -1,6 +1,25 @@
 package ready
 
+import (
+	"fmt"
+
+	"CoinAI.net/server/global"
+	"CoinAI.net/server/global/dbData"
+)
+
 func Start() {
 	GetUserInfo()
 	GetOkxKey()
+
+	if len(dbData.CoinServe.OkxKeyID) < 5 {
+		errStr := fmt.Errorf("读取 dbData.CoinServe 失败 %+v", dbData.CoinServe)
+		global.LogErr(errStr)
+		panic(errStr)
+	}
+
+	if len(dbData.UserInfo.UserID) < 5 {
+		errStr := fmt.Errorf("读取 dbData.UserInfo 失败 %+v", dbData.UserInfo)
+		global.LogErr(errStr)
+		panic(errStr)
+	}
 }
