399402b1777fdd27cee9c51fbc1b30ac5b10d5e5
墨七 <meichangliang@mo7.cc>
2022-08-03 02:24:07 +0800

    exit-push

--- main.go
+++ main.go
@@ -5,10 +5,8 @@ import (
 
 	"CoinAI.net/server/global"
 	"CoinAI.net/server/global/config"
-	"CoinAI.net/server/okxApi/wssApi"
 	"CoinAI.net/server/ready"
 	"CoinAI.net/server/router"
-	"github.com/EasyGolang/goTools/mStr"
 	jsoniter "github.com/json-iterator/go"
 )
 
@@ -21,17 +19,6 @@ func main() {
 	global.Start()
 
 	ready.Start()
-
-	wss := wssApi.New(wssApi.FetchOpt{
-		Type: 1,
-		Event: func(s string, a any) {
-			global.WssLog.Println("Event", s, mStr.ToStr(a))
-		},
-	})
-
-	go wss.Read(func(msg []byte) {
-		global.WssLog.Println("读数据", mStr.ToStr(msg))
-	})
 
 	router.Start()
 }
