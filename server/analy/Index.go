a9f50a81abbe761ebadaa3217755e1ba79bd0ddc
墨七 <meichangliang@mo7.cc>
2022-08-17 17:28:05 +0800

    exit-push

--- server/analy/Index.go
+++ server/analy/Index.go
@@ -6,37 +6,17 @@ import (
 )
 
 func MarketStart() {
+	// 清空全局的排行榜单
+	okxInfo.Hour8Ticker = []mOKX.AnalySliceType{}
+	okxInfo.Hour8TickerUR = []mOKX.AnalySliceType{}
+
 	// 一旦有一个长度不对，则 Market 不合格
 	if len(okxInfo.Unit) < 3 || len(okxInfo.TickerList) < 4 || len(okxInfo.AnalyWhole) < 4 || len(okxInfo.AnalySingle) < 4 {
 		return
 	}
 
-	// 清空全局的排行榜单
-	okxInfo.Hour8Ticker = []mOKX.AnalySliceType{}
-	okxInfo.Hour8TickerUR = []mOKX.AnalySliceType{}
-
-	// 将 8 小时切片提取出来，做一个排名
-	Hour8Ticker := []mOKX.AnalySliceType{}
-	for _, item := range okxInfo.AnalySingle {
-		for _, Slice := range item {
-			if Slice.DiffHour == 8 {
-				Hour8Ticker = append(Hour8Ticker, Slice)
-			}
-		}
-	}
-	// 按照成交量排序
-	Hour8TickerVolSort := mOKX.SortAnalySlice_Volume(Hour8Ticker)
-	// 前 1/2
-	MaxLen := len(Hour8TickerVolSort) / 2
-	Hour8TickerVol := Hour8TickerVolSort[0:MaxLen]
-
-	// copy 一份
-	Filter8Ticker := make([]mOKX.AnalySliceType, len(Hour8TickerVol))
-	copy(Filter8Ticker, Hour8TickerVol)
-
-	// 成交量排序 前 1/2
-	okxInfo.Hour8Ticker = Filter8Ticker
-
-	// 成交量排序前 1/2  的涨跌幅排序
-	okxInfo.Hour8TickerUR = mOKX.SortAnalySlice_UR(Filter8Ticker)
+	// 设置 近 8 小时成交量的榜单
+	SetHour8Ticker()
+	// 判断市场大盘
+	JudgeTrendDir()
 }
