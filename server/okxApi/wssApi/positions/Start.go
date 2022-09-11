949a6d8491eb6957be09357825dd3564dd06bf9c
墨七 <meichangliang@mo7.cc>
2022-09-11 14:06:18 +0800

    exit-push

--- server/okxApi/wssApi/positions/Start.go
+++ server/okxApi/wssApi/positions/Start.go
@@ -1,6 +1,7 @@
 package positions
 
 import (
+	"strings"
 	"time"
 
 	"CoinAI.net/server/global"
@@ -19,7 +20,7 @@ func Start() {
 		Event: func(lType string, a any) {
 			cont := mStr.ToStr(a)
 			if cont == "pong" || cont == "ping" {
-				global.WssLog.Println("positions.Start1", lType, cont)
+				// global.WssLog.Println("positions.Start1", lType, cont)
 				return
 			}
 
@@ -30,7 +31,7 @@ func Start() {
 			}
 
 			if lType == "Close" {
-				global.WssLog.Println("positions.Start4", lType, cont, "链接关闭,执行重启")
+				global.WssLog.Println("positions.Start4", lType, cont)
 				time.Sleep(time.Second * 2)
 				Start()
 				return
@@ -44,16 +45,16 @@ func Start() {
 	})
 
 	Wss.Read(func(msg []byte) {
-		// global.WssLog.Println("positions.Start5", "wss.Read", mStr.ToStr(msg))
+		global.WssLog.Println("positions.Start5", mStr.ToStr(msg))
 
-		// cont := mStr.ToStr(msg)
-		// find := strings.Contains(cont, "login") // 是否为SWAP
-		// if find {
-		// isLogin := Read_Login(msg)
-		// if !isLogin {
-		// wss.Close("登录失败")
-		// return
-		// }
-		// }
+		cont := mStr.ToStr(msg)
+		find := strings.Contains(cont, "login") // 登录是否成功
+		if find {
+			isLogin := Read_Login(msg)
+			if !isLogin {
+				Wss.Close("登录失败")
+				return
+			}
+		}
 	})
 }
