0a196b2ce0b0b35d9867a89c0314238fea5a2538
墨七 <meichangliang@mo7.cc>
2022-09-12 15:56:23 +0800

    exit-push

--- server/router/api/sys/Dominate.go
+++ server/router/api/sys/Dominate.go
@@ -11,5 +11,15 @@ func Dominate(c *fiber.Ctx) error {
 	var json SysAuthParam
 	mFiber.Parser(c, &json)
 
+	/*
+		0. 确认当前主机是否有主
+		x. 确认当前的主机是否绑定Key
+		1. 拿到 当前 Token
+		2. 提取 UserID
+		3. 去数据库 读取用户信息并验证密码
+		4. 密码验证通过则验证填写的 Okx 信息
+
+	*/
+
 	return c.JSON(result.Succeed.WithMsg("认主模式"))
 }
