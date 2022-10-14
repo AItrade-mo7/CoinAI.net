f5ad9fcf9b59bdd47c60d2559bb1ddb585b9aa62
墨七 <meichangliang@mo7.cc>
2022-10-14 10:58:11 +0800

    exit-push

--- server/global/EnvInit.go
+++ server/global/EnvInit.go
@@ -8,23 +8,14 @@ import (
 	"CoinAI.net/server/tmpl"
 	"github.com/EasyGolang/goTools/mFile"
 	"github.com/EasyGolang/goTools/mJson"
-	"github.com/EasyGolang/goTools/mPath"
 	"github.com/spf13/viper"
 )
 
 func AppEnvInit() {
 	// 检查配置文件在不在
-	isUserEnvPath := mPath.Exists(config.File.AppEnv)
-	if !isUserEnvPath {
-		WriteAppEnv()
-	}
 
 	viper.SetConfigFile(config.File.AppEnv)
 
-	err := viper.ReadInConfig()
-	if err != nil {
-		LogErr("AppEnv 读取配置文件出错:", err)
-	}
 	viper.Unmarshal(&config.AppEnv)
 
 	if len(config.AppEnv.Port) < 1 {
