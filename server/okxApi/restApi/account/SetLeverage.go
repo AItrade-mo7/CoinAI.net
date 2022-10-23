eb904f15d740bfb6dd7ca2638136702f7006f9b3
墨七 <meichangliang@mo7.cc>
2022-10-23 19:17:55 +0800

    exit-push

--- server/okxApi/restApi/account/SetLeverage.go
+++ server/okxApi/restApi/account/SetLeverage.go
@@ -8,22 +8,40 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
-// type SetLeverageParam struct {
-// 	 InstID
-// }
+type SetLeverageParam struct {
+	InstID string
+	OKXKey mOKX.TypeOkxKey
+	Lever  int
+}
 
-func SetLeverage(OKXKey mOKX.TypeOkxKey) (resErr error) {
+func SetLeverage(opt SetLeverageParam) (resErr error) {
+	resErr = nil
+
+	if len(opt.InstID) < 3 {
+		resErr = fmt.Errorf("opt.InstID 不能为空 %+v", opt.InstID)
+		return
+	}
+	if opt.Lever < 1 {
+		resErr = fmt.Errorf("opt.Lever 不能为0 %+v", opt.Lever)
+		return
+	}
+	if len(opt.OKXKey.ApiKey) < 10 {
+		resErr = fmt.Errorf("opt.OKXKey.ApiKey 不能为空 %+v", opt.OKXKey.ApiKey)
+		return
+	}
+
 	res, err := mOKX.FetchOKX(mOKX.OptFetchOKX{
 		Path:   "/api/v5/account/set-leverage",
 		Method: "POST",
-		OKXKey: OKXKey,
+		OKXKey: opt.OKXKey,
 		Data: map[string]any{
-			"instId": "net_mode",
+			"instId":  opt.InstID,
+			"lever":   mStr.ToStr(opt.Lever),
+			"mgnMode": "cross",
 		},
 	})
 	if err != nil {
 		resErr = err
-
 		return
 	}
 
