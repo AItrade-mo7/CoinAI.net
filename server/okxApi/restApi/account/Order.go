e93d2db0ab857c3fae0c24d47440432b6f34ab84
墨七 <meichangliang@mo7.cc>
2022-10-23 19:37:55 +0800

    exit-push

--- server/okxApi/restApi/account/Order.go
+++ server/okxApi/restApi/account/Order.go
@@ -2,6 +2,7 @@ package account
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/EasyGolang/goTools/mOKX"
 	"github.com/EasyGolang/goTools/mStr"
@@ -11,6 +12,9 @@ import (
 type OrderParam struct {
 	InstID string
 	OKXKey mOKX.TypeOkxKey
+	OrdId  string
+	Side   string // buy  sell
+	Sz     string
 }
 
 func Order(opt OrderParam) (resErr error) {
@@ -24,14 +28,32 @@ func Order(opt OrderParam) (resErr error) {
 		resErr = fmt.Errorf("account.Order opt.OKXKey.ApiKey 不能为空 %+v", opt.OKXKey.ApiKey)
 		return
 	}
+	if opt.Side == "buy" || opt.Side == "sell" {
+	} else {
+		resErr = fmt.Errorf("account.Order opt.Side 不正确 %+v", opt.Side)
+		return
+	}
+
+	tdMode := "cash" // 币币交易
+	ordType := "market"
+	find := strings.Contains(opt.InstID, "-SWAP")
+	if find {
+		tdMode = "cross" // 全仓杠杆
+		ordType = "optimal_limit_ioc"
+	}
 
 	res, err := mOKX.FetchOKX(mOKX.OptFetchOKX{
 		Path:   "/api/v5/trade/order",
 		Method: "POST",
 		OKXKey: opt.OKXKey,
 		Data: map[string]any{
-			"instId": opt.InstID,
-			"tdMode": "cross",
+			"instId":  opt.InstID,
+			"tdMode":  tdMode,
+			"clOrdId": opt.OrdId,
+			"side":    opt.Side,
+			"posSide": "net",
+			"ordType": ordType,
+			"sz":      opt.Sz,
 		},
 	})
 	if err != nil {
