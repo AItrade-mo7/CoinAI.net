58d93511bb542908036a48a40cd5b7b7c5aff2ea
墨七 <meichangliang@mo7.cc>
2022-10-26 03:34:39 +0800

    exit-push

--- server/okxApi/restApi/account/Order.go
+++ server/okxApi/restApi/account/Order.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"CoinAI.net/server/global"
+	"github.com/EasyGolang/goTools/mCount"
 	"github.com/EasyGolang/goTools/mOKX"
 	jsoniter "github.com/json-iterator/go"
 )
@@ -15,6 +16,7 @@ type OrderParam struct {
 	OrdId     string
 	Side      string // buy  sell
 	Sz        string
+	MaxEvent  func()
 }
 
 // 下单接口
@@ -26,11 +28,22 @@ func Order(opt OrderParam) (resErr error) {
 		global.LogErr(resErr)
 		return
 	}
+	if mCount.Le(opt.TradeInst.MinSz, "0") < 1 {
+		resErr = fmt.Errorf("account.Order opt.TradeInst.MinSz 不能为空 %+v", opt.TradeInst.MinSz)
+		global.LogErr(resErr)
+		return
+	}
+
 	if len(opt.OKXKey.ApiKey) < 10 {
 		resErr = fmt.Errorf("account.Order opt.OKXKey.ApiKey 不能为空 %+v", opt.OKXKey.ApiKey)
 		global.LogErr(resErr)
 		return
 	}
+
+	if opt.MaxEvent == nil {
+		opt.MaxEvent = func() {}
+	}
+
 	if opt.Side == "buy" || opt.Side == "sell" {
 	} else {
 		resErr = fmt.Errorf("account.Order opt.Side 不正确 %+v", opt.Side)
@@ -46,11 +59,19 @@ func Order(opt OrderParam) (resErr error) {
 		ordType = "optimal_limit_ioc"
 	}
 
-	fmt.Println(opt.TradeInst.InstID)
-	fmt.Println(opt.Side)
-	fmt.Println(opt.Sz)
-	fmt.Println(opt.TradeInst.MaxMktSz)
-	fmt.Println(opt.TradeInst.MinSz)
+	// 小于最小数量
+	if mCount.Le(opt.Sz, opt.TradeInst.MinSz) < 0 {
+		resErr = fmt.Errorf("交易数量太小 %+v", opt.Sz)
+		global.LogErr(resErr)
+		return
+	}
+
+	// 大于最大数量 则 最大数乘以 0.8
+	if mCount.Le(opt.Sz, opt.TradeInst.MaxMktSz) > 0 {
+		opt.Sz = mCount.Mul(opt.TradeInst.MaxMktSz, "0.8")
+	}
+
+	opt.Sz = mCount.Cent(opt.Sz, 0)
 
 	res, err := mOKX.FetchOKX(mOKX.OptFetchOKX{
 		Path:   "/api/v5/trade/order",
@@ -79,5 +100,6 @@ func Order(opt OrderParam) (resErr error) {
 		global.LogErr(resErr)
 		return
 	}
+
 	return
 }
