25514ebcbb6f3338831e24c50e65f2190d80f087
墨七aws <mo7@mo7.cc>
2022-12-07 17:10:53 +0800

    exit-push

--- server/router/api/SetKey.go
+++ server/router/api/SetKey.go
@@ -68,7 +68,7 @@ func SetKey(c *fiber.Ctx) error {
 	ApiKey.IsTrade = true
 	ApiKey.UserID = UserID
 
-	// 验证 Key 准确性
+	// 验证 Key 可用性
 	_, err = account.GetOKXBalance(ApiKey)
 	if err != nil {
 		return c.JSON(result.ErrLogin.WithMsg("Api Key 验证失败!"))
