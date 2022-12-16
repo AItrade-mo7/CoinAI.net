1f28fe790441a8b2fedd3811f294a81abf125019
墨七aws <mo7@mo7.cc>
2022-12-16 10:08:25 +0800

    exit-push

--- server/utils/backTest/TestRun.go
+++ server/utils/backTest/TestRun.go
@@ -190,8 +190,8 @@ func Analy() {
 	RsiRegion_Up := hunter.Is_RsiRegion_GoUp(PreList)
 	RsiRegion_Gte2 := hunter.Is_RsiRegion_Gte2(PreList)
 
-	CAP_EMA_GoDown := hunter.Is_CAP_EMA_GoDown(PreList)
-	CAP_EMA_GoUp := hunter.Is_CAP_EMA_GoUp(PreList)
+	// CAP_EMA_GoDown := hunter.Is_CAP_EMA_GoDown(PreList)
+	// CAP_EMA_GoUp := hunter.Is_CAP_EMA_GoUp(PreList)
 
 	RsiRegionDir := 0
 	if RsiRegion_Down {
@@ -203,10 +203,9 @@ func Analy() {
 
 	// 主调 CAPIdx
 	if lastIdx != preIdx {
-		if Last.CAPIdx > 0 {
+		if Last.CAPIdx > 0 { // Buy
 			// 包括当前在内 RsiRegion 是为升序 // 且 在过去一段时间 RsiRegion 内存在 非 1 的情况
-			if CAP_EMA_GoUp {
-				// Buy
+			if RsiRegion_Up {
 				global.TradeLog.Printf(
 					"%v %4v RSI:%2v %8v RsiDir: %2v Gte2: %5v Pre0: %8v CAP_EMA: %8v  \n",
 					Last.TimeStr,
@@ -223,8 +222,7 @@ func Analy() {
 		}
 
 		if Last.CAPIdx < 0 { // sell
-			if CAP_EMA_GoDown {
-				// Sell
+			if RsiRegion_Down {
 				global.TradeLog.Printf(
 					"%v %4v RSI:%2v %8v RsiDir: %2v Gte2: %5v Pre0: %8v CAP_EMA: %8v \n",
 					Last.TimeStr,
@@ -242,21 +240,35 @@ func Analy() {
 	}
 
 	// 在这里进行防火作业
-	// CAPIdxAbs := mCount.Abs(fmt.Sprint(Last.CAPIdx))
-	// if mCount.Le(CAPIdxAbs, "2") >= 0 && Last.CAPIdx == Last.RsiRegion {
-	// 	global.TradeLog.Printf(
-	// 		"%v %4v RSI:%2v %8v RsiDir: %2v Gte2: %5v Pre0: %8v CAP_EMA: %8v  \n",
-	// 		Last.TimeStr,
-	// 		lastIdx+fmt.Sprint(Last.CAPIdx),
-	// 		Last.RsiRegion,
-	// 		Last.RSI_18,
-	// 		RsiRegionDir,
-	// 		RsiRegion_Gte2,
-	// 		PreList[0].RSI_18,
-	// 		Last.CAP_EMA,
-	// 	)
-	// 	return
-	// }
+	if Last.CAPIdx > 0 && RsiRegionDir > 0 {
+		global.TradeLog.Printf(
+			"%v %4v RSI:%2v %8v RsiDir: %2v Gte2: %5v Pre0: %8v CAP_EMA: %8v  \n",
+			Last.TimeStr,
+			lastIdx+fmt.Sprint(Last.CAPIdx),
+			Last.RsiRegion,
+			Last.RSI_18,
+			RsiRegionDir,
+			RsiRegion_Gte2,
+			PreList[0].RSI_18,
+			Last.CAP_EMA,
+		)
+		return
+	}
+
+	if Last.CAPIdx < 0 && RsiRegionDir < 0 {
+		global.TradeLog.Printf(
+			"%v %4v RSI:%2v %8v RsiDir: %2v Gte2: %5v Pre0: %8v CAP_EMA: %8v  \n",
+			Last.TimeStr,
+			lastIdx+fmt.Sprint(Last.CAPIdx),
+			Last.RsiRegion,
+			Last.RSI_18,
+			RsiRegionDir,
+			RsiRegion_Gte2,
+			PreList[0].RSI_18,
+			Last.CAP_EMA,
+		)
+		return
+	}
 
 	global.TradeLog.Printf(
 		"%v %4v RSI:%2v %8v RsiDir: %2v Gte2: %5v Pre0: %8v  CAP_EMA: %8v  \n",
