e5032ce6cf909b80fcd8d78522b945eddc539f56
墨七 <mo7@mo7.cc>
2023-03-09 14:40:59 +0800

    exit-push

--- ReBoot.sh
+++ ReBoot.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-startFilePath="/root/AItrade/CoinAI.net/jsonData/Reboot.txt"
-sudo cat >${startFilePath} <<END
-#!/bin/bash
-time3=$(date "+%Y-%m-%d %H:%M:%S")
 
 END
--- Shutdown.sh
+++ Shutdown.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-startFilePath="/root/AItrade/CoinAI.net/jsonData/Shutdown.txt"
-sudo cat >${startFilePath} <<END
-#!/bin/bash
-time3=$(date "+%Y-%m-%d %H:%M:%S")
 
 END
