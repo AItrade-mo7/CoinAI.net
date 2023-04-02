eba29e63138ec3efd801dc972f6efbf47aad714c
墨七 <mo7@mo7.cc>
2023-04-03 01:12:09 +0800

    x

--- task/task.go
+++ task/task.go
@@ -8,6 +8,7 @@ import (
 	"CoinAI.net/server/global"
 	"CoinAI.net/server/global/config"
 	"CoinAI.net/server/hunter"
+	"CoinAI.net/server/utils/taskPush"
 	"CoinAI.net/task/testHunter"
 	"github.com/EasyGolang/goTools/mJson"
 	"github.com/EasyGolang/goTools/mStr"
@@ -72,22 +73,20 @@ func main() {
 
 	configArr := MockConfig([]int{77, 171, 545})
 
-	global.Run.Println(mJson.Format(configArr))
-
-	// for _, config := range configArr {
-	// 	back.MockData(
-	// 		config.MockOpt,
-	// 		config.TradeKdataOpt,
-	// 	)
-	// }
-	// taskPush.SysEmail(taskPush.SysEmailOpt{
-	// 	From:        config.SysName,
-	// 	To:          config.NoticeEmail,
-	// 	Subject:     "参数跑完了",
-	// 	Title:       "第一批参数组合跑完了",
-	// 	Content:     "参数值:" + mJson.Format(configArr),
-	// 	Description: "回测结束通知",
-	// })
+	for _, config := range configArr {
+		back.MockData(
+			config.MockOpt,
+			config.TradeKdataOpt,
+		)
+	}
+	taskPush.SysEmail(taskPush.SysEmailOpt{
+		From:        config.SysName,
+		To:          config.NoticeEmail,
+		Subject:     "参数跑完了",
+		Title:       "第一批参数组合跑完了",
+		Content:     "参数值:" + mJson.Format(configArr),
+		Description: "回测结束通知",
+	})
 }
 
 /*
