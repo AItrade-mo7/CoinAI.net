2159b857d3d025c5fa756c2a381fe6efe9dcb2e3
墨七 <mo7@mo7.cc>
2023-04-03 01:10:22 +0800

    参数组合

--- task/task.go
+++ task/task.go
@@ -9,6 +9,7 @@ import (
 	"CoinAI.net/server/global/config"
 	"CoinAI.net/server/hunter"
 	"CoinAI.net/task/testHunter"
+	"github.com/EasyGolang/goTools/mJson"
 	"github.com/EasyGolang/goTools/mStr"
 	"github.com/EasyGolang/goTools/mTime"
 	jsoniter "github.com/json-iterator/go"
@@ -22,23 +23,27 @@ type MockOptType struct {
 func MockConfig(EmaPArr []int) []MockOptType {
 	MockConfigArr := []MockOptType{}
 
+	CAPArr := []int{2, 3, 4, 5, 6}
+
 	for _, emaP := range EmaPArr {
-		MockConfigArr = append(MockConfigArr,
-			MockOptType{
-				testHunter.BillingType{
-					MockName:  "EMA_" + mStr.ToStr(emaP),
-					InitMoney: "1000", // 初始资金
-					Level:     "1",    // 杠杆倍数
-					Charge:    "0.05", // 吃单标准手续费率 0.05%
+		for _, cap := range CAPArr {
+			MockConfigArr = append(MockConfigArr,
+				MockOptType{
+					testHunter.BillingType{
+						MockName:  "EMA_" + mStr.ToStr(emaP) + "_CAP_" + mStr.ToStr(cap),
+						InitMoney: "1000", // 初始资金
+						Level:     "1",    // 杠杆倍数
+						Charge:    "0.05", // 吃单标准手续费率 0.05%
+					},
+					hunter.TradeKdataOpt{
+						MA_Period:      emaP,
+						RSI_Period:     18,
+						RSI_EMA_Period: 14,
+						CAP_Period:     cap,
+					},
 				},
-				hunter.TradeKdataOpt{
-					MA_Period:      emaP,
-					RSI_Period:     18,
-					RSI_EMA_Period: 14,
-					CAP_Period:     3,
-				},
-			},
-		)
+			)
+		}
 	}
 
 	return MockConfigArr
@@ -65,23 +70,10 @@ func main() {
 		fmt.Println("出错", err)
 	}
 
-	// configArr := MockConfig([]int{76, 78})
-	//  	for _, config := range configArr {
-	//  		back.MockData(
-	//  			config.MockOpt,
-	//  			config.TradeKdataOpt,
-	//  		)
-	//  	}
-	//  	taskPush.SysEmail(taskPush.SysEmailOpt{
-	//  		From:        config.SysName,
-	//  		To:          config.NoticeEmail,
-	//  		Subject:     "参数跑完了",
-	//  		Title:       "第一批参数组合跑完了",
-	//  		Content:     "参数值:" + mJson.Format(configArr),
-	//  		Description: "回测结束通知",
-	//  	})
+	configArr := MockConfig([]int{77, 171, 545})
 
-	// configArr := MockConfig([]int{170, 172})
+	global.Run.Println(mJson.Format(configArr))
+
 	// for _, config := range configArr {
 	// 	back.MockData(
 	// 		config.MockOpt,
@@ -92,28 +84,10 @@ func main() {
 	// 	From:        config.SysName,
 	// 	To:          config.NoticeEmail,
 	// 	Subject:     "参数跑完了",
-	// 	Title:       "第二批参数组合跑完了",
+	// 	Title:       "第一批参数组合跑完了",
 	// 	Content:     "参数值:" + mJson.Format(configArr),
 	// 	Description: "回测结束通知",
 	// })
-
-	// configArr := MockConfig([]int{544, 546})
-	// for _, config := range configArr {
-	// 	back.MockData(
-	// 		config.MockOpt,
-	// 		config.TradeKdataOpt,
-	// 	)
-	// }
-	// taskPush.SysEmail(taskPush.SysEmailOpt{
-	// 	From:        config.SysName,
-	// 	To:          config.NoticeEmail,
-	// 	Subject:     "参数跑完了",
-	// 	Title:       "第三批参数组合跑完了",
-	// 	Content:     "参数值:" + mJson.Format(configArr),
-	// 	Description: "回测结束通知",
-	// })
-
-	select {}
 }
 
 /*
