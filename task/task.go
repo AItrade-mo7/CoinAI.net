68d7af93ab5e05105be151d77f8f1ad125a9052b
墨七 <mo7@mo7.cc>
2023-04-04 17:26:58 +0800

    任务多线程系统

--- task/task.go
+++ task/task.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	_ "embed"
-	"os"
 
 	"CoinAI.net/server/global"
-	"CoinAI.net/server/global/config"
 	"CoinAI.net/task/taskStart"
-	jsoniter "github.com/json-iterator/go"
 )
 
 func main() {
-	AppPackage, _ := os.ReadFile("package.json")
-	jsoniter.Unmarshal(AppPackage, &config.AppInfo)
 	// 初始化系统参数
 	global.Start()
 
