6adda290b05b9d42315dfff42ffe69cb2d1d6fbd
墨七 <mo7@mo7.cc>
2023-04-09 14:45:54 +0800

    保存

--- task/task.go
+++ task/task.go
@@ -6,7 +6,8 @@ import (
 	"CoinAI.net/server/global"
 	"CoinAI.net/task/analyConfig"
 	"CoinAI.net/task/taskStart"
-	"github.com/EasyGolang/goTools/mStr"
+	"CoinAI.net/task/testHunter"
+	"github.com/EasyGolang/goTools/mTime"
 )
 
 var ResultBasePath = "/root/AItrade/CoinAI.net/task/analyConfig/data"
@@ -15,38 +16,52 @@ func main() {
 	// 初始化系统参数
 	global.Start()
 
-	// BackBtc := taskStart.BackTest(taskStart.BackOpt{
-	// 	StartTime: 3132131,
-	// 	EndTime:   31313,
-	// 	InstID:    "BTC-USDT",
-	// 	GetConfigOpt: testHunter.GetConfigOpt{
-	// 		EmaPArr:  []int{}, // Ema 步长
-	// 		CAPArr:   []int{}, // CAP 步长
-	// 		LevelArr: []int{}, // 杠杆倍数
-	// 	},
-	// })
+	BackAnaly()
+}
+
+func BackAnaly() {
+	EndTime := mTime.GetUnixInt64()
+	StartTime := EndTime - (mTime.UnixTimeInt64.Day * 260)
 
 	InstID := "BTC-USDT"
-	analyConfig.GetWinArr(taskStart.BackReturn{
-		InstID:         InstID,
-		BillingPath:    mStr.Join(ResultBasePath, "/", InstID, "-BillingArr.json"),
-		ResultBasePath: ResultBasePath,
+	BTCResult := taskStart.BackTest(taskStart.BackOpt{
+		StartTime: StartTime,
+		EndTime:   EndTime,
+		InstID:    InstID,
+		GetConfigOpt: testHunter.GetConfigOpt{
+			EmaPArr:  []int{76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88},
+			CAPArr:   []int{2, 3, 4, 5},
+			LevelArr: []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
 	})
-	// BackEth := taskStart.BackTest(taskStart.BackOpt{
-	// 	StartTime: 3132131,
-	// 	EndTime:   31313,
-	// 	InstID:    "BTC-USDT",
-	// 	GetConfigOpt: testHunter.GetConfigOpt{
-	// 		EmaPArr:  []int{}, // Ema 步长
-	// 		CAPArr:   []int{}, // CAP 步长
-	// 		LevelArr: []int{}, // 杠杆倍数
-	// 	},
-	// })
+	BTCResult.ResultBasePath = ResultBasePath
+	analyConfig.GetWinArr(
+		BTCResult,
+		// taskStart.BackReturn{
+		// 	InstID:         InstID,
+		// 	BillingPath:    mStr.Join(ResultBasePath, "/", InstID, "-BillingArr.json"),
+		// 	ResultBasePath: ResultBasePath,
+		// },
+	)
 
 	InstID = "ETH-USDT"
-	analyConfig.GetWinArr(taskStart.BackReturn{
-		InstID:         InstID,
-		BillingPath:    mStr.Join(ResultBasePath, "/", InstID, "-BillingArr.json"),
-		ResultBasePath: ResultBasePath,
+	ETHResult := taskStart.BackTest(taskStart.BackOpt{
+		StartTime: StartTime,
+		EndTime:   EndTime,
+		InstID:    InstID,
+		GetConfigOpt: testHunter.GetConfigOpt{
+			EmaPArr:  []int{76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88},
+			CAPArr:   []int{2, 3, 4, 5},
+			LevelArr: []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
 	})
+	ETHResult.ResultBasePath = ResultBasePath
+	analyConfig.GetWinArr(
+		ETHResult,
+		// taskStart.BackReturn{
+		// 	InstID:         InstID,
+		// 	BillingPath:    mStr.Join(ResultBasePath, "/", InstID, "-BillingArr.json"),
+		// 	ResultBasePath: ResultBasePath,
+		// },
+	)
 }
