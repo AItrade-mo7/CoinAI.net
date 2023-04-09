aaf71a84a99f7230c8f5cab2decd41495b16a9e1
墨七 <mo7@mo7.cc>
2023-04-10 06:05:04 +0800

    x

--- server/hunter/Analy.go
+++ server/hunter/Analy.go
@@ -45,12 +45,14 @@ func (_this *HunterObj) Analy() {
 
 func (_this *HunterObj) CountPosition() {
 	NowKTradeData := _this.TradeKdataList[len(_this.TradeKdataList)-1]
+
+	_this.NowVirtualPosition.InstID = NowKTradeData.InstID
 	_this.NowVirtualPosition.HunterName = _this.HunterName
 	_this.NowVirtualPosition.NowTimeStr = NowKTradeData.TimeStr
 	_this.NowVirtualPosition.NowTime = mTime.GetTime().TimeUnix
 	_this.NowVirtualPosition.NowC = NowKTradeData.C
 	_this.NowVirtualPosition.CAP_EMA = NowKTradeData.CAP_EMA
-	_this.NowVirtualPosition.InstID = NowKTradeData.InstID
+	_this.NowVirtualPosition.EMA = NowKTradeData.EMA
 	_this.NowVirtualPosition.HunterConfig = NowKTradeData.Opt
 
 	if _this.NowVirtualPosition.NowDir != 0 { // 当前为持仓状态，则计算收益率
@@ -58,43 +60,7 @@ func (_this *HunterObj) CountPosition() {
 		if _this.NowVirtualPosition.NowDir < 0 { // 当前为持空仓状态则翻转该值
 			UplRatio = mCount.Sub("0", UplRatio)
 		}
-		_this.NowVirtualPosition.NowUplRatio = mCount.Mul(UplRatio, mStr.ToStr(_this.TradeKdataOpt.MaxTradeLever)) // 乘以杠杆倍数
-		// 这里应该在客户端执行 金钱收益  Money * UplRatio = NowMoney
+		Level := _this.NowVirtualPosition.HunterConfig.MaxTradeLever
+		_this.NowVirtualPosition.NowUplRatio = mCount.Mul(UplRatio, mStr.ToStr(Level)) // 乘以杠杆倍数
 	}
-}
-
-func (_this *HunterObj) OnOrder(dir int) {
-	NowKTradeData := _this.TradeKdataList[len(_this.TradeKdataList)-1]
-
-	_this.NowVirtualPosition.OpenAvgPx = NowKTradeData.C
-	_this.NowVirtualPosition.OpenTimeStr = NowKTradeData.TimeStr
-	_this.NowVirtualPosition.OpenTime = mTime.GetTime().TimeUnix
-	// 在这里计算当前的 Money
-	Upl := mCount.Div(_this.NowVirtualPosition.NowUplRatio, "100")     // 格式化收益率
-	ChargeUpl := mCount.Div(_this.NowVirtualPosition.ChargeUpl, "100") // 格式化手续费率
-
-	Money := _this.NowVirtualPosition.Money   // 提取 Money
-	makeMoney := mCount.Mul(Money, Upl)       // 当前盈利的金钱
-	nowCharge := mCount.Mul(Money, ChargeUpl) // 当前产生的手续费
-	Money = mCount.Add(Money, makeMoney)      // 相加得出当账户总资金量
-	Money = mCount.Sub(Money, nowCharge)      // 减去手续费
-	Money = mCount.CentRound(Money, 3)        // 四舍五入保留三位小数
-	_this.NowVirtualPosition.Money = Money    // 保存结果到当前持仓
-
-	if dir > 0 {
-		// 开多
-		_this.NowVirtualPosition.NowDir = 1
-	}
-
-	if dir < 0 {
-		// 开空
-		_this.NowVirtualPosition.NowDir = -1
-	}
-
-	if dir == 0 {
-		// 平仓
-		_this.NowVirtualPosition.NowDir = 0
-	}
-	// 平仓后未实现盈亏重置为 0
-	_this.NowVirtualPosition.NowUplRatio = "0"
 }
