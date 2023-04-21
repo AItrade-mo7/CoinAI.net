e110ad26f224da39b270936d3574ff96779e621e
墨七 <mo7@mo7.cc>
2023-04-21 12:34:05 +0800

    x

--- server/hunter/Analy.go
+++ server/hunter/Analy.go
@@ -67,6 +67,12 @@ func GetAnalyDir(NowKTradeData okxInfo.TradeKdType, NowPosition dbType.VirtualPo
 	CAP_Max := NowKTradeData.Opt.CAP_Max // Max 边界值
 	CAP_Min := NowKTradeData.Opt.CAP_Min // Min 边界值
 
+	if NowPosition.NowDir != 0 {
+		// 表示当前正在持仓，持仓状态下收窄 边界值
+		CAP_Max = mCount.Add(CAP_Max, "0")
+		CAP_Min = mCount.Add(CAP_Min, "0")
+	}
+
 	CountDir := 0
 	if mCount.Le(CAP_EMA, CAP_Max) > 0 { // 大于 Max 则向上
 		CountDir = 1
