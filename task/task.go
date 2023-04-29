9d6fdd040034adbed042f4c5c20bdf948c03b7a6
墨七 <mo7@mo7.cc>
2023-04-29 13:27:56 +0800

    x

--- task/task.go
+++ task/task.go
@@ -5,14 +5,15 @@ import (
 	"fmt"
 
 	"CoinAI.net/server/global"
-	"CoinAI.net/server/global/dbType"
 	"CoinAI.net/server/hunter/testHunter"
 	"CoinAI.net/task/taskHunter"
+	"github.com/EasyGolang/goTools/mFile"
+	"github.com/EasyGolang/goTools/mJson"
 	"github.com/EasyGolang/goTools/mStr"
 	"github.com/EasyGolang/goTools/mTime"
 )
 
-var ResultBasePath = "/root/AItrade/CoinAI.net/task/2021_2022"
+var ResultBasePath = "/root/AItrade/CoinAI.net/task/2022_2023"
 
 func main() {
 	// 初始化系统参数
@@ -20,11 +21,11 @@ func main() {
 
 	// Step1("BTC-USDT")
 	// Step2("BTC-USDT")
-	// Step3("BTC-USDT")
+	Step3("BTC-USDT")
 	// Step4("BTC-USDT")
 
 	// Step1("ETH-USDT")
-	Step2("ETH-USDT")
+	// Step2("ETH-USDT")
 	// Step3("ETH-USDT")
 	// Step4("ETH-USDT")
 }
@@ -82,49 +83,51 @@ func Step2(InstID string) {
 
 func Step3(InstID string) {
 	// 第三步：提取第二步的配置，加上杠杆得出新的参数组合 大概 几百个 然后 换个新的时间段进行新一轮测试
-	// confArr := analyConfig.GetWinConfig(analyConfig.GetWinConfigOpt{
-	// 	OutPutDir: ResultBasePath,
-	// 	InstID:    InstID,
-	// })
+	confArr := taskHunter.GetWinConfig(taskHunter.GetWinConfigOpt{
+		OutPutDir: ResultBasePath,
+		InstID:    InstID,
+	})
 
 	// 提取 EMA 的值
+
+	EmaArr := []int{}
 
 	// ConfArr := []dbType.TradeKdataOpt{}
 	// LevelArr := []int{2, 3, 4, 5}
 
-	// for _, conf := range confArr {
-	// 	for _, l := range LevelArr {
-	// 		conf.MaxTradeLever = l
-	// 		ConfArr = append(ConfArr, conf)
-	// 	}
-	// }
+	for _, conf := range confArr {
+		EmaArr = append(EmaArr, conf.EMA_Period)
+	}
+	mFile.Write(mStr.Join(
+		ResultBasePath, "/", InstID, "-EmaArr.json",
+	), mJson.ToStr(EmaArr))
 
 	// 新一轮求解，计算最优杠杆倍率 用  2022 年 8 月 的 260 天前进行回测 （此步骤会更换时间段反复进行）
 	// EndTime := mTime.TimeParse(mTime.Lay_DD, "2022-10-01")
-	EndTime := mTime.TimeParse(mTime.Lay_DD, "2023-05-01")
-	StartTime := EndTime - (mTime.UnixTimeInt64.Day * 260)
-	taskHunter.BackTest(taskHunter.BackOpt{
-		StartTime: StartTime,
-		EndTime:   EndTime,
-		InstID:    InstID,
-		OutPutDir: mStr.Join(ResultBasePath, "/Step3"),
-		GetConfigOpt: testHunter.GetConfigOpt{
-			// EmaPArr:  []int{342},      // Ema 步长
-			// CAPArr:   []int{7},        // CAP 步长
-			// CAPMax:   []string{"2.5"}, // CAPMax 步长
-			// CAPMin:   []string{"-2.5"},
-			// LevelArr: []int{2},
-			ConfArr: []dbType.TradeKdataOpt{
-				{
-					EMA_Period:    342,
-					CAP_Period:    7,
-					CAP_Max:       "2.5",
-					CAP_Min:       "-2.5",
-					MaxTradeLever: 5,
-				},
-			},
-		},
-	})
+	// EndTime := mTime.TimeParse(mTime.Lay_DD, "2023-05-01")
+	// StartTime := EndTime - (mTime.UnixTimeInt64.Day * 260)
+	// taskHunter.BackTest(taskHunter.BackOpt{
+	// 	StartTime: StartTime,
+	// 	EndTime:   EndTime,
+	// 	InstID:    InstID,
+	// 	OutPutDir: mStr.Join(ResultBasePath, "/Step3"),
+	// 	GetConfigOpt: testHunter.GetConfigOpt{
+	// 		// EmaPArr:  []int{342},      // Ema 步长
+	// 		// CAPArr:   []int{7},        // CAP 步长
+	// 		// CAPMax:   []string{"2.5"}, // CAPMax 步长
+	// 		// CAPMin:   []string{"-2.5"},
+	// 		// LevelArr: []int{2},
+	// 		ConfArr: []dbType.TradeKdataOpt{
+	// 			{
+	// 				EMA_Period:    342,
+	// 				CAP_Period:    7,
+	// 				CAP_Max:       "2.5",
+	// 				CAP_Min:       "-2.5",
+	// 				MaxTradeLever: 5,
+	// 			},
+	// 		},
+	// 	},
+	// })
 }
 
 func Step4(InstID string) {
