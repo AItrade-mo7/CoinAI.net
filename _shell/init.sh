fa78b902f64cdbadf1ec4b450d14c5fc092dba9e
墨七 <mo7@mo7.cc>
2023-05-16 17:31:16 +0800

    x

--- _shell/init.sh
+++ _shell/init.sh
@@ -14,27 +14,23 @@ function GitSet {
 # 项目根目录
 NowPath=$(pwd)
 
-# 项目的名字和编译时的名字
-StartName=${NowPath##*/}
-BuildName=${StartName}
-
 # 最终的输出目录
 OutPutPath="${NowPath}/dist"
 
 # 远程仓库目录地址
 DeployPath="git@github.com:AItrade-mo7/CoinAIPackage.git"
 
-# 远程仓库的本地目录
-DeployLocalPath="${NowPath}/CoinAIPackage"
+# Deploy 完成之后的输出
+DeployEndText="
+https://github.com/AItrade-mo7/CoinAIPackage
+"
 
 echo "
 ===== 初始化变量 =====
 
 当前目录: ${NowPath}
-项目的名字: ${StartName}
-编译后的二进制包的名字: ${BuildName}
 输出目录: ${OutPutPath}
 远程仓库目录: ${DeployPath}
-远程仓库本地临时目录: ${DeployLocalPath}
+完成之后的输出: ${DeployEndText}
 
 "
